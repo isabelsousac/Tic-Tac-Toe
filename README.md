930a33397dd0c2267a6193c6b170f4d9fcae3fb7
isabelsousac <isabel.sousa15246@gmail.com>
2022-10-14 02:28:30 +1100

    It know works on small screen. When the player wins or ends as a draw, a message appears and the board is cleaned.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Tic - Tac - Toe
+
+![Game Board](/img/game-board.png?raw=true "Game Board") ---- pelo amor de deus MUDA AUI
+
+## Game description 
+Twoplayers take turns putting marks on the board. The goal of Tic Tac Toe game is to be one of the players to get three same symbols in a row - horizontally, vertically or diagonally on a 3 x 3 grid. 
+Attempt to line up 3 crosses or 3 noughts before your opponent, taking alternating turns.
+Hover over the the location you would like to make your move and click to place your piece.
+
+## Play at the link below:
+
+https://anico94.github.io/Tic-Tac-Toe/
+
+## Coding Languages
+ * HTML 
+ * CSS
+ * Javascript
+
+## Libraries used
+* [jQuery](https://jquery.com/) - Event handling
+* [Lodash](https://lodash.com/) - Array comparison
+* [Animate.css](https://animate.style/) - Winning animation
+* [CSS Scan](https://getcssscan.com/css-buttons-examples) - Button Styling
+
+## Functionality to come .....
+Addtion of a 1 player game against the computer is coming soon with optional difficulties for the AI.
+
+## Acknowledgements
+* Thank you to [Loden Gendun](https://github.com/Tenzang) for all the help and support to visit his github I have attached the link to his name.
+* Thank you to [The Blade](https://github.com/wofockham) to visit his github I have attached the link to his name.
--- game/js/dom.js
+++ game/js/dom.js
@@ -4,32 +4,34 @@ function checkEndGame() {
     if (tictactoeGame.checkForDraw()) {
         console.log(possibleEnds.DRAW);
         buildDrawEnd();
+        $(".cell").off("click", markCell);
     }
     if (tictactoeGame.checkForWin()) {
         console.log(possibleEnds.WIN);
+        $(".cell").off("click", markCell);
         buildWinEnd();
     }
     tictactoeGame.increaseMovementNumber();
+    $(".turn-icon").html(tictactoeGame.getPlayer().icon);
 }
 
 function clearField() {
+    $(".end-game").removeClass("show");
+    $(".turn").removeClass("hide");
     tictactoeGame.clearField();
     matchHTMLCells();
     updateHTML();
+    $(".cell").on("click", markCell);
 }
 
 function updatePoints($container) {
     const previousPoint = Number($container.html());
     $container.html(previousPoint + 1);
+    $(".turn-icon").html(tictactoeGame.getPlayer().icon);
 }
 
 function buildWinEnd() {
     const winnerPlayer = tictactoeGame.getPlayer();
-
-    const $winEndGameDiv = $(`<div class=end-game">`);
-    ($(`<p>YAY</p>`)).appendTo($winEndGameDiv);
-    ($(winnerPlayer.icon)).appendTo($winEndGameDiv);
-    $("bottom-page").prepend($winEndGameDiv);
 
     if (winnerPlayer.icon === possibleIcons.X_ICON) {
         updatePoints($(".xPoints p"));
@@ -37,19 +39,21 @@ function buildWinEnd() {
         updatePoints($(".oPoints p"));
     }
 
-    setTimeout(clearField, 5000);
-    $winEndGameDiv.remove();
+    $(".turn").addClass("hide");
+    $(".end-game-phrase").html("YAY");
+    $(".end-icon").html(winnerPlayer.icon);
+    $(".end-game").addClass("show");
+    setTimeout(clearField, 3000);
 }
 
 function buildDrawEnd() {
-    const $drawEndGameDiv = $(`<div class="end-game">`);
-    $("<p>It's a draw</p>").appendTo($drawEndGameDiv);
-    $(`<img src="../images/draw.png">`).appendTo($drawEndGameDiv);
-    $("bottom-page").prepend($drawEndGameDiv);
+    updatePoints($(".draw p"));
+    $(".turn").addClass("hide");
+    $(".end-game-phrase").html("TRY HARDER");
+    $(".end-icon").html($(`<img src="../images/draw.png">`));
+    $(".end-game").addClass("show");
 
-    updatePoints($(".draw p"))
-    setTimeout(clearField, 5000);
-    $drawEndGameDiv.remove();
+    setTimeout(clearField, 3000);
 }
 
 function updateHTML() {
@@ -92,14 +96,29 @@ function getGridSize(gridSize) {
 }
 
 function addGameContainerCSS(gridSize, $gameContainer) {
-    const gridHeight = $gameContainer.height();
+    const gridHeight = $(window).height()
     const cellSize = ((gridHeight / gridSize) / 100 * 10);
     $gameContainer.css({
-        "display": "grid",
+        // "display": "grid",
         "grid-template-rows": `repeat(${gridSize}, ${cellSize}%)`,
         "grid-template-columns": `repeat(${gridSize}, ${cellSize}%)`,
         "place-content": "center"
     })
+}
+
+function removeBorders($cellDiv, x, y, gridSize) {
+    if (y === 0) {
+        $cellDiv.addClass("no-border-top");
+    }
+    if (x === 0) {
+        $cellDiv.addClass("no-border-left");
+    }
+    if (x === gridSize - 1) {
+        $cellDiv.addClass("no-border-rigth");
+    }
+    if (y === gridSize - 1) {
+        $cellDiv.addClass("no-border-bottom");
+    }
 }
 
 function buildHTMLGame(gridSize) {
@@ -108,6 +127,8 @@ function buildHTMLGame(gridSize) {
         for (let x = 0; x < gridSize; x++) {
             const $newDiv = $(`<div class="cell" id="${y}x${x}">`);
             $newDiv.appendTo($gameContainer);
+
+            removeBorders($newDiv, x, y, gridSize);
         }
     }
     addGameContainerCSS(gridSize, $gameContainer);
@@ -122,6 +143,6 @@ $(document).ready(function() {
     buildHTMLGame(gridSize);
 
     matchHTMLCells();
-
+    $(".turn-icon").html(tictactoeGame.getPlayer().icon);
     $(".cell").on("click", markCell);
 });
