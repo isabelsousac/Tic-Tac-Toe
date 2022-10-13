aa1bb1c8fdc41b288f651efde92638f962649199
isabelsousac <isabel.sousa15246@gmail.com>
2022-10-14 07:50:33 +1100

    fix img src

--- game/js/enums.js
+++ game/js/enums.js
@@ -1,6 +1,6 @@
 const possibleIcons = {
-    X_ICON: `<img src="/Users/isabelsousa/Projects_GA/Projects/project0/images/xIcon.png" class="player-icon">`,
-    O_ICON: `<img src="/Users/isabelsousa/Projects_GA/Projects/project0/images/oIcon.png" class="player-icon">`,
+    X_ICON: `<img src="../images/xIcon.png" class="player-icon">`,
+    O_ICON: `<img src="../images/oIcon.png" class="player-icon">`,
     NOT_PLAYED_ICON: ""
 }
 
